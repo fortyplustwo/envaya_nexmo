6d6200bc2fbed3c05fed0563cac45924980ebd1a
Samarendra M Hedaoo <knightsamar@gmail.com>
2014-07-19 09:28:18 +0530

    A simple SMS sending test for NexmoOutgoingBackend

--- tests.py
+++ tests.py
@@ -1,16 +1,23 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+class NexmoOutgoingBackendTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_sending_sms(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that SMS-es are actually sent using the Nexmo outgoing backend.
         """
-        self.assertEqual(1 + 1, 2)
+        try:
+            from django.conf import settings
+        except ImportError:
+            self.fail(msg="No TEST_NUMBER found in settings!")
+
+        from rapidsms.router import send
+        from rapidsms.models import Connection, Backend
+        from random import randint
+
+        b = Backend.objects.get_or_create(name='envaya_nexmo')[0]
+        c = Connection.objects.get_or_create(identity = settings.TEST_NUMBER, backend = b)[0]
+        msg = "Hey, this is a test message from NexmoOutgoingBackendTest! \n Your Lucky number is %s" % (randint(1,42))
+
+        send(msg,[c])
+        print "Cannot actually verify whether the message was sent or not because of the limitations of rapdisms framework :-/"
